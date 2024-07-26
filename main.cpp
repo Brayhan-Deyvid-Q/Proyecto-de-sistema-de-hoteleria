ebf3100af4376f4f379cb747eda28a461b369895
Jose <jvilcanquic@unjbg.edu.pe>
2024-07-25 23:19:55 -0500

    Se modifico el menu principal

--- main.cpp
+++ main.cpp
@@ -3,28 +3,19 @@
 using namespace std; 
 
 int main(){
-	
-	//Reserva reservahabitacion[200];//declaración de variable para registro de clientes
-	
-	int contclientes=0; //contador de clientes
-	int opcion, opcion2;
-	SetConsoleOutputCP(CP_UTF8);
+	//Reserva reservahabitacion[30];//declaración de variable para registro de clientes
+	int opcion;
 	
 	cout<<"\n----BIENVENIDOS AL HOTEL EL 'TELO'----\n"<<endl;
 	
-	while(opcion!=9){ //Impresión del menú en la pantalla
+	while(opcion!=4){ //Impresión del menú en la pantalla
 	
-	    cout<<"Seleccione una opción:"<<endl<<endl;
-	    cout<<"1) Registrarse"<<endl
-	        <<"2) Reserva de habitación"<<endl
-	        <<"3) Servicio de limpieza"<<endl
-	        <<"4) Restaurant"<<endl
-	        <<"5) Estacionamiento"<<endl
-	        <<"6) Actualizar datos"<<endl
-	        <<"7) Eliminar datos"<<endl
-	        <<"8) Historial de reservas"<<endl
-	        <<"9) Salir"<<endl<<endl;
-	    cout<<"Opción: ";
+	    cout<<"Seleccione una opcion:"<<endl<<endl;
+	    cout<<"1) Cliente"<<endl;
+	    cout<<"2) Habitaciones" << endl;
+	    cout<<"3) Reserva" << endl;
+	    cout<<"4) Salir del programa" << endl << endl;
+	    cout<<"Opcion: ";
 	    cin>>opcion;
 	    cout<<endl;
 	    
@@ -32,40 +23,10 @@ int main(){
 	    	case 1:
 	    		break;
 	    	case 2:
-	    		while(opcion2!=3){
-	    		cout<<"1) Visualizar habitaciones disponibles"<<endl;
-	    		cout<<"2) Hacer una reserva"<<endl;
-	    		cout<<"3) Volver al menú principal"<<endl;
-	    		cout<<"Opción: ";
-	    		cin>>opcion2;
-	    		cout<<endl;
-	    		}
-	    		switch (opcion2){
-	    			case 1:
-	    				break;
-	    			case 2:
-	    				//registroReserva(reservahabitacion, contclientes);
-	    				break;
-	    			case 3:
-						break; 	
-	    			default: 
-	    				cout<<"ERROR: Alternativa no válida, intente de nuevo..."<<endl<<endl;
-	    				break;
-				}
 	    		break;
 	    	case 3:
 	    		break;
 	    	case 4:
-	    		break;
-	    	case 5:
-	    		break;
-	    	case 6:
-	    		break;
-	    	case 7:
-	    		break;
-	    	case 8:
-	    		break;
-	    	case 9:
 	    		cout<<"Saliendo...";
 	    		break;
 	    	default:
