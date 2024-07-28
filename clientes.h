a2bc7ffe5ca6e6792430e10971bbe04a4bd11bd3
Jose <jvilcanquic@unjbg.edu.pe>
2024-07-28 15:15:55 -0500

    Muda de estructuras en la librería de clientes.h

--- clientes.h
+++ clientes.h
@@ -1,6 +1,19 @@
 #ifndef HUESPED_H
 #define HUESPED_H
 
-void huespedHotel();
+struct Habitaciones {
+    int num_habi;
+    char tipo[20];
+};
+
+struct Huesped {
+    char Nombre[30];
+    char Telefono[10];
+    char dni[10];
+    struct Habitaciones hab;
+};
+void huespedHotel(int &n_huesp, Huesped huespedes[30]);
+
+
 
 #endif
