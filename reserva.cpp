2e0e79aced9b5394ec6156e3ce2639f75f138c26
unknown <bdquispec@unjbg.edu.pe>
2024-07-28 01:30:36 -0500

    Añadiendo función de validación de fecha

--- reserva.cpp
+++ reserva.cpp
@@ -2,9 +2,30 @@
 #include "reserva.h"
 using namespace std;
 
+void validacionFecha(int dia, int mes, int anio){
+	do{
+		
+		if (mes<1 || mes>12){
+			cout << "El mes ingresado no es válida, por favor ingrese un dato real." << endl; 
+		}else if(dia<1 || dia>31){
+			cout << "El día ingresado no es valida, por favor ingrese un dato real." << endl;
+		}else if((mes==4 || mes==6 || mes==9 || mes==11)&& dia>30){
+			cout << "La fecha ingresada no es valida, por favor ingrese un dato real." << endl;
+		}else if(mes==2){
+			if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0){
+				if(dia >29){
+					cout << "La fecha ingresada no es valida, por favor ingrese un dato real." << endl;
+				}
+			}
+		}else if(dia>28){
+			cout << "La fecha ingresada no es valdia, por favor ingrese un valor real." << endl; 
+		}
+	}while(mes<1 || mes>12 || dia<1 || dia>31 ||((mes==4 || mes==6 || mes==9 || mes==11)&& dia>30)||(mes==2 &&((anio%4==0 && anio%100!=0)||anio%400==0)?dia>29 : dia>28));
+}
+
 void reservaHotel(){
 
-	int opcion;
+	int opcion, dia, mes, anio;
     do {
     	cout << "\n" << endl;
         cout << "1. Seleccionar cliente" << endl;
