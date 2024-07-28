b0ed5bd4a6453505267507d021428133eade2e61
Jose <jvilcanquic@unjbg.edu.pe>
2024-07-28 12:24:00 -0500

    Muda de estructuras a biblioteca

--- habitacions.cpp
+++ habitacions.cpp
@@ -1,45 +1,27 @@
-#include<iostream>
+/*#include<iostream>
 #include "habitaciones.h"
 #include <windows.h>
 using namespace std;
 
 
-struct Habitaciones{
-	char nombre[20];
-	int num_habi;
-	char tipo[20];
-}hab[30] = {{"Franco Avalos", 1, "Triple"},
-					{"Daniela Alvarez", 2, "Individual"},
-					{"Camila Roque", 3, "Doble"},
-					{"David Mamani", 4, "Doble"},
-					{"Mariela Herrera", 5, "Individual"},
-					{"Diego Rivera", 6, "Triple"},
-					{"Martina Benitez", 7, "Invidual"},
-					{"Alexander Chicaya", 8, "Triple"}
-					
-					};
 
-int ha_actu = 8; //habitaciones actuales
 
-int buscarHuesped(Habitaciones ha[], int n){
+int buscarHuesped(Habitaciones huespedes[], int n){
 	for(int i = 0; i<30; i++){
-		if ( ha[i].num_habi == n){
+		if ( huespedes[i].hab.num_habi == n){
 			return i;
 		}
 	}
 	return -1;
 }
 
-void agregarHabitacion(){
-	Habitaciones h;
+void agregarHabitacion(int &n_huesp, Huesped huespedes[]){
 	
 	cin.ignore();
 	
-	cout << "Digite el nombre del huesped: " << endl;
-	cin.getline(hab[ha_actu].nombre, 20);
 	
 	cout << "Digite el número de habitación del huesped (9-30): " << endl;
-	cin >> hab[ha_actu].num_habi;
+	cin >> huespedes[ha_actu].hab.num_habi;
 	
 	cin.ignore();
 	
@@ -47,7 +29,7 @@ void agregarHabitacion(){
 	cout << "-Individual" << endl;
 	cout << "-Doble" << endl;
 	cout << "-Triple" << endl;
-	cin.getline(hab[ha_actu].tipo, 20);
+	cin.getline(huespedes[ha_actu].hab.tipo, 20);
 	
 	cout << "Habitacion agregada" << endl;
 	ha_actu++;
@@ -56,40 +38,38 @@ void agregarHabitacion(){
 void listarHabitaciones(){
 	for ( int i = 0; i< ha_actu; i++){
 		
-		cout << "Nombre: " << hab[i].nombre << endl;
-		cout << "Numero de habitacion: " << hab[i].num_habi << endl;
-		cout << "Tipo de habitacion: " << hab[i].tipo << endl << endl;
+		cout << "Numero de habitacion: " << huespedes[i].hab.num_habi << endl;
+		cout << "Tipo de habitacion: " << huespedes[i].hab.tipo << endl << endl;
 		cout <<"---------------------------------------------" << endl;
 	}
 
 }
-void actualizarHabitacion(){
+void actualizarHabitacion(Habitaciones){
 	int num;
 	
 	cout << "Digite el numero de habitacion que va editar: " << endl;
 	cin >> num;
 	cout << endl;
 	
-	int numero = buscarHuesped(hab, num);
+	int numero = buscarHuesped(huespedes, num);
 	
 	if (numero != -1){
 		cout << "Huesped encontrado" << endl;
-		cout << "Nombre: " << hab[numero].nombre << endl;
-		cout << "Numero de habitacion: " << hab[numero].num_habi << endl;
-		cout << "Tipo de habitacion: " << hab[numero].tipo << endl << endl;
+		cout << "Numero de habitacion: " << huespedes[numero].hab.num_habi << endl;
+		cout << "Tipo de habitacion: " << huespedes[numero].hab.tipo << endl << endl;
 		
 		cout << "Digite el nuevo numero de habitacion: " << endl;
-		cin >> hab[numero].num_habi;
+		cin >> huespedes[numero].hab.num_habi;
 		cout << "Digite el nuevo tipo de habitacion: " << endl;
-		cin >> hab[numero].tipo;
+		cin >> huespedes[numero].hab.tipo;
 		
 		cout << "Los datos fueron actualizados" << endl;
 	} else{
 		cout << "Huesped no encontrado." << endl;
 	}
-}				
+}			
 
-void habitacionesHotel(){
+void habitacionesHotel(int &n_huesp, Huesped huespedes[], Habitaciones){
 	
 	int opcion;
     do {
@@ -103,11 +83,11 @@ void habitacionesHotel(){
         cin >> opcion;
         
         cout << endl;
-        // SWITCH PARA SELECIONAR LAS DIFERENTES OPCIONES DEL MENU
+        SWITCH PARA SELECIONAR LAS DIFERENTES OPCIONES DEL MENU
         switch (opcion) {
            
             case 1:
-                agregarHabitacion();
+                agregarHabitacion(int &n_huesp, Huesped huespedes[]);
 
                 break;
             case 2:
@@ -115,7 +95,7 @@ void habitacionesHotel(){
             	
                 break;
             case 3: 
-            	actualizarHabitacion();
+            	//actualizarHabitacion(Habitaciones);
             	break;
             case 4:
             	break;
@@ -128,4 +108,4 @@ void habitacionesHotel(){
         }
         
     } while (opcion != 5); //EN CASO SELECCIONAR EL NUMERO 4 REGRESA AL MENU ANTERIOR
-}
+}*/
